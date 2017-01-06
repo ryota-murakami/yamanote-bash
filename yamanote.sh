5fdc3db5866a56a8480555326c4c3ea1f9eb2d7c
ryota-murakami <dojce1048@gmail.com>
2017-01-06 22:06:55 +0900

    fix log message

--- yamanote.sh
+++ yamanote.sh
@@ -2,15 +2,15 @@
 
 # 山手線に入力された駅があるか探す
 in_stations() {
-  stations=("東京" "有楽町" "新橋" "浜松町" "田町" "品川" "大崎" "五反田" "目黒" "恵比寿" "渋谷" "原宿" "代々木" "新宿" "新大久保" "高田馬場" "目白" "池袋" "大塚" "巣鴨" "駒込" "田端" "西日暮里" "日暮里" "鶯谷" "上野" "御徒町" "秋葉原" "神田") 
+  stations=("東京" "有楽町" "新橋" "浜松町" "田町" "品川" "大崎" "五反田" "目黒" "恵比寿" "渋谷" "原宿" "代々木" "新宿" "新大久保" "高田馬場" "目白" "池袋" "大塚" "巣鴨" "駒込" "田端" "西日暮里" "日暮里" "鶯谷" "上野" "御徒町" "秋葉原" "神田")
 
   for station in ${stations[@]};do
     # 正解の場合入力値を配列に格納
     if [ $1 = $station ];then
-      echo "${1} clear!!"
-      cleared=("${cleared[@]}" ${1})
+      echo "${1} done!!"
+      done=("${done[@]}" ${1})
       # 全て正解した場合はスクリプトを終了
-      if [ "${#cleared[@]}" -eq 29 ];then
+      if [ "${#done[@]}" -eq 29 ];then
         exit
       fi
       return 0
@@ -22,8 +22,8 @@ in_stations() {
 }
 
 # 既に入力された駅名はスキップする
-in_cleared() {
-  for clear in ${cleared[@]};do
+in_done() {
+  for clear in ${done[@]};do
     if [ $1 = $clear ];then
       return 0
     fi
@@ -35,13 +35,13 @@ in_cleared() {
 echo "Let's play yamanote-game!!"
 echo "Please press the Enter after typing the station name of the Yamanote Line"
 echo "Example 東京 有楽町 新橋 etc.."
-cleared=()
+done=()
 while :
 do
   read input
-  in_cleared $input
+  in_done $input
   if [ $? -eq 0 ];then
-    echo "${input} is already cleared"
+    echo "${input} is already done"
     continue
   fi
   in_stations $input
